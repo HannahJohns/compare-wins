ec820e0c3e7160a3faee52a4888a98ffb9165aae
Hannah Johns <34296560+HannahJohns@users.noreply.github.com>
2025-05-05 17:00:20 +1000

    Hotfix for if shiny is missing for some reason when we go to run the app
    NOTE: This does not resolve issues with installing on ARM architecture (e.g. surface pro), because that requires compilation of dependency packages in order to work. That would require bundling RTools as well, which is overkill

--- source/app/app.R
+++ source/app/app.R
@@ -1,1 +1,6 @@
+if(!"shiny" %in% rownames(installed.packages())){
+  # This command will fail if shiny is not installed. If so, install it now.
+  install.packages("shiny",repos = "https://cloud.r-project.org/")
+}
+
 shiny::runApp('./app/shiny',launch.browser = TRUE)
