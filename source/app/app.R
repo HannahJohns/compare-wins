24c4f3bd7f0e2ad18c1ec23df4d59ec31805b507
Hannah Johns <34296560+HannahJohns@users.noreply.github.com>
2025-05-06 11:30:53 +1000

    Release still broken, here's an attempted fix. To test when we get home.

--- source/app/app.R
+++ source/app/app.R
@@ -1,6 +1,15 @@
+
+
+cat("Library paths are:")
+print(.libPaths())
+
+# Set default CRAN mirror for if we need to install anything
+chooseCRANmirror(ind=1)
+
 if(!"shiny" %in% rownames(installed.packages())){
+
   # This command will fail if shiny is not installed. If so, install it now.
-  install.packages("shiny",repos = "https://cloud.r-project.org/")
+  install.packages("shiny", dependencies = T)
 }
 
 shiny::runApp('./app/shiny',launch.browser = TRUE)
--- source/app/shiny/server.R
+++ source/app/shiny/server.R
@@ -1,4 +1,7 @@
 
+# Set default CRAN mirror for if we need to install anything
+chooseCRANmirror(ind=1)
+
 source("R/load_packages.R")
 
 source("R/misc_functions.R")
@@ -17,9 +20,9 @@ module_list <- module_list[grepl("module-[[:alnum:][:space:]_-]+.R",module_list,
 # last-minute substitution of variables here.
 write("Loading server module expressions:", stderr())
 module_server_expr <- sapply(module_list,\(f){
-  
+
   write(sprintf("\t%s",f), stderr())
-  
+
   this_module <- source(sprintf("R/module-ROOT/%s",f))$value
   server_element <- rlang::expr_text(this_module$server_element)
 
@@ -51,25 +54,25 @@ inputCollection <- function(input_names,input_index){
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
+
   session$onSessionEnded(function() {
     cat("Session ended",file=stderr())
     stopApp()
   })
-  
+
   # Insert module server code
   eval(module_server_expr)
-  
+
   # Check for updates. This happens after modules (including settings) are loaded
   # so we know if we should bypass this.
-  
+
   # See module-SETTINGS.R for definition of SETTINGS__settings()
   update_flagged <- check_update(software_version,isolate(SETTINGS__settings()))
   heartbeat(software_version,isolate(SETTINGS__settings()))
-  
+
   observe({
     update_flagged
-    
+
     if(update_flagged$needed){
       showModal(modalDialog(
         title=sprintf("Update available: %s",update_flagged$new_tag),
@@ -81,13 +84,13 @@ server <- function(input, output, session) {
           ),
           target="_blank"
         )
-        
+
       ))
     }
   })
-  
+
   output$software_version_display <- renderText(
     sprintf("v%s",paste(software_version,collapse="."))
   )
-  
+
 }
